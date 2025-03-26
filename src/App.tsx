53ee432c2bb7a21dc0de12cfb5135a561121e5ee
t-sats <tetiana.satsuk@gmail.com>
2025-03-26 16:02:33 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './style.css';
+
+function App() {
+
+  return (
+    <>
+      <div className='container'>
+        <h1>React + TypeScript Todo List</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
